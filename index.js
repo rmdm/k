81ba338185cc0e91f717b9a735afd935a32e66b6
d <d@dpc>
2019-03-22 00:06:21 +0300

    quite a few features

--- index.js
+++ index.js
@@ -8,37 +8,11 @@ import data from './data/chart_data.json'
 
 const factory = new ChartFactory(Panel, Canvas, Scrollbar, Checkboxes)
 
-const panels = []
-
 document.addEventListener('DOMContentLoaded', function () {
 
     const parent = document.getElementById('root')
 
     for (let datum of data) {
-        panels.push(factory.create(datum, parent))
-    }
-
-    window.moveLeft = function (left) {
-        for (let panel of panels) {
-            panel.canvas.moveLeft(left)
-        }
-    }
-
-    window.moveRight = function (right) {
-        for (let panel of panels) {
-            panel.canvas.moveRight(right)
-        }
-    }
-
-    window.disable = function (label) {
-        for (let panel of panels) {
-            panel.canvas.disable(label)
-        }
-    }
-
-    window.enable = function (label) {
-        for (let panel of panels) {
-            panel.canvas.enable(label)
-        }
+        factory.create(datum, parent)
     }
 })

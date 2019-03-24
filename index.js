d6d409aa3aca32fb5930485fafecde514fecab04
d <d@dpc>
2019-03-24 21:07:42 +0300

    .

--- index.js
+++ index.js
@@ -3,16 +3,40 @@ import Canvas from './lib/canvas'
 import Checkboxes from './lib/checkboxes'
 import Scrollbar from './lib/scrollbar'
 import ChartFactory from './lib/chart_factory'
+import NightSwitch from './lib/night_switch'
 
 import data from './data/chart_data.json'
 
 const factory = new ChartFactory(Panel, Canvas, Scrollbar, Checkboxes)
+
+let panels = []
 
 document.addEventListener('DOMContentLoaded', function () {
 
     const parent = document.getElementById('root')
 
     for (let datum of data) {
-        factory.create(datum, parent)
+        panels.push(factory.create(datum, parent))
     }
+
+    function switchMode (enable) {
+        panels.forEach(function (panel) {
+            panel.nightMode(enable)
+        })
+    }
+
+    const nightSwitch = new NightSwitch(switchMode)
+    parent.appendChild(nightSwitch.el)
+
+    function scale (width) {
+        panels.forEach(function (panel) {
+            panel.scale(width)
+        })
+    }
+
+    window.addEventListener('resize', function () {
+        scale(parent.offsetWidth)
+    })
+
+    scale(parent.offsetWidth)
 })
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: ['./index.js'],
+    output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+    },
+    optimization: {
+    minimizer: [new UglifyJsPlugin({ sourceMap: true })],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    }
+                }
+            }
+        ]
+    },
+    devtool: 'source-map',
+}
